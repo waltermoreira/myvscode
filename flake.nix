07d3b6d1db3f8c5ab03acadf4f8c74a05ef9a72c
Walter Moreira <walter@waltermoreira.net>
2023-03-30 22:35:20 -0500

    Rearrange installables

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "My VSCode";
-  inputs = {    
+  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     shell-utils.url = "github:waltermoreira/shell-utils";
@@ -12,38 +12,46 @@
     , flake-utils
     , shell-utils
     }:
-      with flake-utils.lib; eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        myVSCode = pkgs:
-          let
-            configuration = pkgs.writeTextFile {
-              name = "settings";
-              text = builtins.readFile ./settings.json;
-              destination = "/share/settings.json";
-            };
-            app = pkgs.writeShellApplication {
-              name = "myvscode";
-              runtimeInputs = [ vscode ];
-              text = ''
-                mkdir -p ~/.myvscode/User
-                ln -sf ${configuration}/share/settings.json ~/.myvscode/User/settings.json
-                ${vscode}/bin/code --user-data-dir ~/.myvscode
-              '';
-            };
-            vscode = pkgs.vscode-with-extensions.override {
-              vscodeExtensions = with pkgs.vscode-extensions; [
-                formulahendry.code-runner
-                rust-lang.rust-analyzer
-              ];
-            };
-          in
-          app;
-        shell = shell-utils.myShell.${system};
-      in
-      { 
-        packages.default = myVSCode pkgs;
-      });
+    let
+      myVSCode = pkgs:
+        let
+          configuration = pkgs.writeTextFile {
+            name = "settings";
+            text = builtins.readFile ./settings.json;
+            destination = "/share/settings.json";
+          };
+          app = pkgs.writeShellApplication {
+            name = "code";
+            runtimeInputs = [ vscode ];
+            text = ''
+              mkdir -p ~/.myvscode/User
+              ln -sf ${configuration}/share/settings.json ~/.myvscode/User/settings.json
+              ${vscode}/bin/code --user-data-dir ~/.myvscode
+            '';
+          };
+          vscode = pkgs.vscode-with-extensions.override {
+            vscodeExtensions = with pkgs.vscode-extensions; [
+              formulahendry.code-runner
+              rust-lang.rust-analyzer
+            ];
+          };
+        in
+        app;
+
+      installables = with flake-utils.lib; eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          shell = shell-utils.myShell.${system};
+          code = myVSCode pkgs;
+        in
+        {
+          packages.default = code;
+          devShells.default = shell {
+            packages = [ code ];
+          };
+        });
+    in
+    installables // { app = myVSCode; };
 }
